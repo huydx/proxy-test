31d5949cfabf80b0eb370bc6d1fb0a85daa4ae25
xuanhuy-do <xuanhuy.do@linecorp.com>
2017-12-07 15:17:12 +0900

    fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,33 @@
-import java.util.concurrent.CompletableFuture;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.linecorp.armeria.client.ClientBuilder;
 import com.linecorp.armeria.client.ClientFactory;
 import com.linecorp.armeria.client.ClientFactoryBuilder;
+import com.linecorp.armeria.client.Clients;
 import com.linecorp.armeria.client.HttpClient;
-import com.linecorp.armeria.common.HttpRequest;
-import com.linecorp.armeria.common.HttpRequestDuplicator;
+import com.linecorp.armeria.common.AggregatedHttpMessage;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.SessionProtocol;
 import com.linecorp.armeria.common.util.EventLoopGroups;
+import com.linecorp.armeria.common.util.SafeCloseable;
 import com.linecorp.armeria.server.HttpService;
 import com.linecorp.armeria.server.ServerBuilder;
-import com.linecorp.armeria.server.ServiceRequestContext;
 
 public class App {
     public static void main(String[] args) {
         ClientFactory clientFactory = new ClientFactoryBuilder()
-                .workerGroup(EventLoopGroups.newEventLoopGroup(1), true)
+                .workerGroup(EventLoopGroups.newEventLoopGroup(100), true)
                 .build();
         ClientBuilder clientBuilder = new ClientBuilder("none+http://127.0.0.1:9998").factory(clientFactory);
         final HttpClient httpClient = clientBuilder.build(HttpClient.class);
+        AtomicInteger count = new AtomicInteger(0);
 
-        ServerBuilder sb =  new ServerBuilder().port(20080, SessionProtocol.HTTP);
-        sb.service("/", new HttpService() {
-            @Override
-            public HttpResponse serve(ServiceRequestContext ctx, HttpRequest req) throws Exception {
-                HttpRequestDuplicator duplicator = new HttpRequestDuplicator(req);
-                HttpRequest dupReq = duplicator.duplicateStream();
-                CompletableFuture<HttpResponse> res = dupReq.aggregate().thenApply( r -> {
-                    return httpClient.execute(r.toHttpRequest());
-                });
-                return HttpResponse.from(res);
-            }
+        ServerBuilder sb =  new ServerBuilder().port(20080, SessionProtocol.HTTP).defaultRequestTimeoutMillis(0);
+        sb.service("/test", (HttpService) (ctx, req) -> {
+            int c = count.incrementAndGet();
+            System.out.println(c);
+            return httpClient.execute(req);
         });
         sb.build().start().join();
     }
